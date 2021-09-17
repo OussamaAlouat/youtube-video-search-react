d3c7e3bcf7429b1ab3ac2ee17b3b663bea3b8dc7
Oussama Alouat <al317257@uji.es>
2021-09-17 08:37:40 +0200

    Set the first loading as ada cardano

--- src/components/App.js
+++ src/components/App.js
@@ -24,6 +24,10 @@ class App extends React.Component {
     })
   }
 
+  componentDidMount() {
+    this.onSearchSubmit('ada cardano')
+  }
+
 
   onVideoSelect = (video) => {
     this.setState({ selectedVideo: video })
