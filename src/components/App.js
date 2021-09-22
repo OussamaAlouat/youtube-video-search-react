ae31b54bc209cf91d632dcc9e0f99ff1238a1047
Oussama Alouat <al317257@uji.es>
2021-09-22 11:59:50 +0200

    Use functional component instead of class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import youtube from '../api/youtube';
 import Header from './Header';
 import SearchBar from './SearchBar';
@@ -7,54 +7,49 @@ import VideoList from './VideoList';
 import './app.css';
 import VideoDetail from './VideoDetail';
 
-class App extends React.Component {
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  state = { videos: [], selectedVideo: null }
-
-  onSearchSubmit = async (term) => {
+  const onSearchSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
           q: term
       }
     });
 
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0]
-    })
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   }
 
-  componentDidMount() {
-    this.onSearchSubmit('ada cardano')
+  useEffect(() => {
+    onSearchSubmit('ada cardano')
+  }, [])
+
+  const onVideoSelect = (video) => {
+    setSelectedVideo(video);
   }
 
-
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video })
-  }
-
-  render() {
-    return(
-      <div>
-        <Header />
-        <div className="container">
-          <div style={{ marginBottom: '10px' }}>
-            <SearchBar onSubmit={ this.onSearchSubmit } />
-          </div>
-          <div className="mt-10 ui grid">
-            <div className="ui row" >
-              <div className="eleven wide column">
-                <VideoDetail video={ this.state.selectedVideo } />
-              </div>
-              <div className="five wide column">
-                <VideoList  videos={ this.state.videos } onVideoSelect={this.onVideoSelect}/>
-              </div>
+  return(
+    <div>
+      <Header />
+      <div className="container">
+        <div style={{ marginBottom: '10px' }}>
+          <SearchBar onSubmit={ onSearchSubmit } />
+        </div>
+        <div className="mt-10 ui grid">
+          <div className="ui row" >
+            <div className="eleven wide column">
+              <VideoDetail video={ selectedVideo } />
+            </div>
+            <div className="five wide column">
+              <VideoList  videos={ videos } onVideoSelect={onVideoSelect}/>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
